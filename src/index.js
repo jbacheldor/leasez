6840417a2b149eff192206e33258b4ae4d5996d8
Jess <jlbachel@umich.edu>
2023-05-22 19:00:07 -0400

    routing is working but currently the pages are showing up under the navigation bar

--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-} from "react-router-dom";
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Home } from './Pages/Home.tsx';
 import { Archive } from './Pages/Archive.tsx';
 import { Compare } from './Pages/Compare.tsx';
-import { Navigation } from './Components/Navigation.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/Archive",
-    element: <Archive/>,
-  },
-  {
-    path: "/Compare",
-    element: <Compare/>,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-    <Navigation/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route exact path='/archive' element={<Archive/>}></Route>
+        <Route exact path='/compare' element={<Compare/>}></Route>
+      </Routes>
+      <App />
     </Router>
-    {/* <App /> */}
   </React.StrictMode>
 );
