b6134c8deb952cee3d4a3f7bcc3203b1fbf849af
Jess <jlbachel@umich.edu>
2023-05-25 17:05:32 -0400

    flag working

---
+++ src/Components/SquareCard/sqaurecard.index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const SquareCard:React.FC = () => {
+
+    
+
+    return (
+        <>
+        </>
+    );
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { SkinnyCard } from "../Components/SkinnyCard/SkinnyCard";
 import "../Components/header.style.css"
 import { mockData } from "../mock-data/Cards";
 import { FilterBar } from "../Components/Filter-Bar/filterbar.index";
+import { SquareCard } from "../Components/SquareCard/sqaurecard.index";
 
 export const Home: React.FC = () => {
+    // include global state & provider here that checks what is selected
+    // if it's square then present square - if not then show skinny
+    const [isSquareView, setView] = useState(false);
 
     return (
     <div className="Home">
     <FilterBar/>
-    <SkinnyCard lease={mockData.SkinnyCardDetailsInput}/>
+    {isSquareView && 
+        <SquareCard/>
+    }
+    {!isSquareView &&
+        <SkinnyCard lease={mockData.SkinnyCardDetailsInput}/>
+    }
     </div>
     );
 }
