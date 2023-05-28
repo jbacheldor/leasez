c4688586c93b010a7d8c3744e48d5dd16286beb6
Jess <jlbachel@umich.edu>
2023-05-27 21:08:53 -0400

    fixed header alignment

--- src/Types/skinnycard.ts
+++ src/Types/skinnycard.ts
@@ -1,13 +1,35 @@
+// need to return and make these not all optional
+// these are the fields that come with every listing and show as a preview
 export interface LeaseBase {
     location?: string,
     streetAddress?: string,
     price?: number,
     beds?: number,
     baths?: number,
+    moveInDate?: string,
+    fee?: boolean,
+    // these are flags not from the original listing
     favorite?: boolean,
     isNew?: boolean,
-    // this is for if/when the listing becomes available
     onMarket?: boolean,
+}
+
+// these are fields that will allow us to filter and search for particular leases
+export interface UnitAmenities extends LeaseBase {
+    washer_dryer: boolean, 
+    privateOutdoorSpace: boolean, 
+    storageAvailable: boolean,
+    dishwasher: boolean,
+}
+
+export interface BuildingAmenities extends LeaseBase {
+    elevator: boolean,
+    doorman: boolean, 
+    laundry: boolean, 
+    gym: boolean,
+    publicOutdoorSpace: boolean,
+    fiosEnabled: boolean,
+    transitLines: [],
 }
 
 export interface SkinnyCardDetailsInput {
