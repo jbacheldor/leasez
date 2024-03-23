3c4d1478b2db92774366f8ec714032574fcad0b5
Jess <jlbachel@umich.edu>
2024-03-22 21:31:50 -0400

    hopefully a fix

---
+++ build/precache-manifest.22e5b3c8e67232abbdcb6b87f3f480de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafbdc312df3a8bb121732a8f0b45095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73e882f242a96a0c28ca",
+    "url": "/static/css/main.7c086c36.chunk.css"
+  },
+  {
+    "revision": "38f1e7f7046b5114791d",
+    "url": "/static/js/2.d1d65b37.chunk.js"
+  },
+  {
+    "revision": "73e882f242a96a0c28ca",
+    "url": "/static/js/main.85d4dd09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
